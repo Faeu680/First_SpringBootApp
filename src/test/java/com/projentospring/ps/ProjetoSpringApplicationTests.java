b8bdd0e922784ee8df04ee857fc913a658d9dd2d
Rafael Ferreira <rafael680ferreira@gmail.com>
2020-12-03 09:52:48 -0300

    Project created

---
+++ src/test/java/com/projentospring/ps/ProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projentospring.ps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
