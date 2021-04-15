0bb06a27486b12fbe14adcea17ae86f88e43aefd
Rafael Ferreira <rafael680ferreira@gmail.com>
2021-04-14 22:15:43 -0300

    Subtotal and Total methods

--- src/main/java/com/projetospring/ps/config/TestConfig.java
+++ src/main/java/com/projetospring/ps/config/TestConfig.java
@@ -63,7 +63,6 @@ public class TestConfig implements CommandLineRunner {
 		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
 		o1.setPayment(pay1);
 		
-//		orderRepository.save(o1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
