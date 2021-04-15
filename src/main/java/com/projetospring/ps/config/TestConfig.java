251bfa6fa30702494827ab6b35c47ab8045b8acb
Rafael Ferreira <rafael680ferreira@gmail.com>
2021-04-14 22:00:28 -0300

    Paymente, one to one association

--- src/main/java/com/projetospring/ps/config/TestConfig.java
+++ src/main/java/com/projetospring/ps/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetospring.ps.entities.Category;
 import com.projetospring.ps.entities.Order;
 import com.projetospring.ps.entities.OrderItem;
+import com.projetospring.ps.entities.Payment;
 import com.projetospring.ps.entities.Product;
 import com.projetospring.ps.entities.User;
 import com.projetospring.ps.entities.enums.OrderStatus;
@@ -58,6 +59,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+//		orderRepository.save(o1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
